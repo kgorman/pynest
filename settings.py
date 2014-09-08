73874fcbeb2a244d7e139b222fa53d0dd27f047f
Kenny Gorman <kgorman@objectrocket.com>
2014-09-08 13:26:37 -0500

    mongodb support

---
+++ settings.py
@@ -0,0 +1,10 @@
+settings = {
+    "nest_username":"<your_nest_username>",
+    "nest_password":"<your_nest_password",
+    "db_table":"nest_data",
+    "db_username":"<your_db_username>",
+    "db_password":"<your_db_password>",
+    "db_host":"<your_db_hostname>",
+    "db_port":<your_db_port>,
+    "db_name":"nest"
+}
